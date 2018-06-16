7aa189b3c95f6ecf61991f2b7406224db4db03de
PENGYUXIONG <pxiong@ualberta.ca>
2018-06-15 21:55:55 -0600

    all

---
+++ app/src/main/java/com/example/pengyuxiong/taxigo/View/Driver_Main_Activity.java
@@ -0,0 +1,15 @@
+package com.example.pengyuxiong.taxigo.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pengyuxiong.taxigo.R;
+
+public class Driver_Main_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver__main_);
+    }
+}
--- app/src/main/java/com/example/pengyuxiong/taxigo/View/Log_In_Activity.java
+++ app/src/main/java/com/example/pengyuxiong/taxigo/View/Log_In_Activity.java
@@ -1,13 +1,15 @@
-package com.example.pengyuxiong.taxigo;
+package com.example.pengyuxiong.taxigo.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.pengyuxiong.taxigo.R;
+
+public class Log_In_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_log_in);
     }
 }
