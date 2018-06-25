a637dfdbe370c4a7eb7deed069450fbd038e63f5
PENGYUXIONG <pxiong@ualberta.ca>
2018-06-25 03:03:53 -0600

    more of  view

--- app/src/main/java/com/example/pengyuxiong/taxigo/View/Log_In_Activity.java
+++ app/src/main/java/com/example/pengyuxiong/taxigo/View/Log_In_Activity.java
@@ -1,15 +1,58 @@
 package com.example.pengyuxiong.taxigo.View;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.pengyuxiong.taxigo.R;
 
 public class Log_In_Activity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button login_button;
+    private Button signup_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        /**
+         *  claim variables for edit_texts and buttons
+         */
+        username = (EditText) findViewById(R.id.Username);
+        password = (EditText) findViewById(R.id.PassWord);
+        login_button = (Button) findViewById(R.id.Login_button);
+        signup_button = (Button) findViewById(R.id.Signup_button);
+
+        /**
+         * If user press login button
+         */
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Username = username.getText().toString();
+                String Password = password.getText().toString();
+            }
+        });
+
+        /**
+         * if user press signup_button
+         */
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Log_In_Activity.this,
+                        Sign_Up_Activity.class);
+                Log_In_Activity.this.startActivityForResult(intent, Sign_Up_Activity.request);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/pengyuxiong/taxigo/View/Sign_Up_Activity.java
+++ app/src/main/java/com/example/pengyuxiong/taxigo/View/Sign_Up_Activity.java
@@ -2,14 +2,76 @@ package com.example.pengyuxiong.taxigo.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.pengyuxiong.taxigo.R;
 
 public class Sign_Up_Activity extends AppCompatActivity {
 
+    public static final int request = 0;
+
+    private EditText phone;
+    private EditText email;
+    private EditText username;
+    private EditText password;
+    private ImageButton Addimage;
+    private Button done;
+    private CheckBox driver;
+    private CheckBox passenger;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign__up_);
+
+        phone = (EditText) findViewById(R.id.Phone);
+        email = (EditText) findViewById(R.id.Email);
+        username = (EditText) findViewById(R.id.Username);
+        password = (EditText) findViewById(R.id.PassWord);
+        done = (Button) findViewById(R.id.Done);
+        driver = (CheckBox) findViewById(R.id.checkBox);
+        passenger = (CheckBox) findViewById(R.id.checkBox2);
+
+
+        /**
+         *  if selected driver
+         */
+        driver.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (driver.isChecked()) {
+                    passenger.setChecked(false);
+                }
+            }
+        });
+
+        /**
+         * if select passenger option
+         */
+        passenger.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (passenger.isChecked()){
+                    driver.setChecked(false);
+                }
+            }
+        });
+
+        /**
+         * if finished sign up
+         */
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int Phone = Integer.parseInt(phone.getText().toString());
+                String Email = email.getText().toString();
+
+                finish();
+            }
+        });
     }
 }
